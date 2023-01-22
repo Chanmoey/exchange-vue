f516117319c78fb2f2bbb0c55e1d4c65b6acc8de
Chanmoey <relijifa@gmail.com>
2023-01-23 02:00:12 +0800

    完成委托下单以及前后端联调。

---
+++ src/api/exchangeApi.js
@@ -0,0 +1,37 @@
+// 资金相关的Api接口
+
+import {getRequest} from "@/api/axiosCommon";
+import store from "@/store";
+
+export const queryBalance = () => {
+    getRequest("/api/balance").then(resp => {
+        if (resp) {
+            store.commit("updateBalance", resp.data)
+        }
+    })
+}
+
+export const queryOrder = () => {
+    getRequest("/api/order").then(resp => {
+        if (resp) {
+            store.commit("updateOrder", resp.data)
+        }
+    })
+}
+
+export const queryTrade = () => {
+    getRequest("/api/trade").then(resp => {
+        if (resp) {
+            store.commit("updateTrade", resp.data)
+        }
+    })
+}
+
+export const queryPosition = () => {
+    getRequest("/api/position").then(resp => {
+        if (resp) {
+            console.log(resp.data)
+            store.commit("updatePosition", resp.data)
+        }
+    })
+}
