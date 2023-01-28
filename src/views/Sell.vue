fe83d2ee468c9d74eae00bffc61b6a6d939444f1
Chanmoey <relijifa@gmail.com>
2023-01-28 12:53:10 +0800

    构建卖出页面，建立与柜台的WebSocket连接，实时接受成交数据。

---
+++ src/views/Sell.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item>
+                    <el-icon>
+                        <ShoppingBag/>
+                    </el-icon>
+                    委托
+                </el-breadcrumb-item>
+                <el-breadcrumb-item>
+                    买入
+                </el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+
+        <el-card shadow="hover" class="container">
+            <el-row>
+                <!--                委托组件-->
+                <el-col :span="12">
+                    <OrderWidget :direction="1"></OrderWidget>
+                </el-col>
+                <!--                订单簿组件-->
+                <el-col :span="12">
+                    <OrderBook></OrderBook>
+                </el-col>
+            </el-row>
+
+            <PosiList></PosiList>
+        </el-card>
+    </div>
+</template>
+
+<script>
+import PosiList from "@/components/PosiList.vue";
+import OrderWidget from "@/components/OrderWidget.vue";
+import OrderBook from "@/components/OrderBook.vue";
+
+export default {
+    name: "buy",
+    components: {
+        PosiList,
+        OrderWidget,
+        OrderBook
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
